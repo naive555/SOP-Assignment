2e87a12db6751e68803de4363b3383c4ffcfee14
Non Ninkham <32705865+naive555@users.noreply.github.com>
2019-09-04 21:04:59 +0700

    Added function.
    Also deleted for requirements.

--- src/main/java/liverpool/cloth/LiverpoolShopManager.java
+++ src/main/java/liverpool/cloth/LiverpoolShopManager.java
@@ -1,7 +1,9 @@
 package liverpool.cloth;
 
+import java.util.*;  
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +15,13 @@ public class LiverpoolShopManager {
 	private Shirt shirt = new Shirt(1, "LFC Mens Charcoal Knitted Crest Tee LFC Mens Charcoal Knitted Crest Tee", 25.00);
 	private Jumpers jumpers = new Jumpers(2, "LFC Mens Colour Block Crew Neck Sweat", 45.00);
 	private JacketsAndOutwears jackets = new JacketsAndOutwears(3, "LFC Mens Green Harrington Jacket", 50.00);
-	private Hoodies hoodies = new Hoodies(4, "LFC Mens Red Marl Liverbird Hoody", 30.00);
-	private TrousersAndShorts trousers = new TrousersAndShorts(5, "LFC Mens Grey Marl Tapered Jog Pants", 20.00);
-	private Underwear underwear = new Underwear(6, "LFC Mens Two Pack Boxed Boxer Shorts", 20.00);
-	private Footwear footwear = new Footwear(7, "NB X LFC 997 Trainer", 85.00);
+	List<LiverpoolShop> cateragory = new ArrayList<LiverpoolShop>(
+			Arrays.asList(
+					shirt,
+					jumpers,
+					jackets
+					)
+			);
 	
 	@RequestMapping("/shirt")
 	public String getShirt() {
@@ -33,23 +38,17 @@ public class LiverpoolShopManager {
 		return "You selected "+jackets.getModel()+", Price: £"+jackets.getPrice();
 	}
 	
-	@RequestMapping("/hoodies")
-	public String getHoodies() {
-		return "You selected "+hoodies.getModel()+", Price: £"+hoodies.getPrice();
+	@RequestMapping("/cart/{id}")
+	public String addCart(@PathVariable int id) {
+		System.out.println(cateragory.size());
+		for (int i=0; i<cateragory.size(); i++) {
+			if (id > cateragory.size() || id < 1) {
+				return "No Cateragory found.";
+			} else if (cateragory.get(i) == cateragory.get(id-1)){
+				return "Added to Cart.";
+			}
+		}
+		return null;
 	}
 	
-	@RequestMapping("/trousers")
-	public String getTrousers() {
-		return "You selected "+trousers.getModel()+", Price: £"+trousers.getPrice();
-	}
-	
-	@RequestMapping("/underwear")
-	public String getUnderwear() {
-		return "You selected "+underwear.getModel()+", Price: £"+underwear.getPrice();
-	}
-	
-	@RequestMapping("/footwear")
-	public String getFootwear() {
-		return "You selected "+footwear.getModel()+", Price: £"+footwear.getPrice();
-	}
 }
